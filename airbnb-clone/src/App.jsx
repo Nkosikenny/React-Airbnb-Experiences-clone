b19dc5572b4a6e21215e5b78b40fe9a4533b50ba
nkosikenny <117735539+Nkosikenny@users.noreply.github.com>
2024-06-20 15:16:28 +0000

    added data file

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -2,22 +2,31 @@ import { useState } from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Card from './components/Card'
+import data from "./components/data"
+
 
 function App() {
-  
+  const cardComponents = data.map(item => {
+    return (<Card
+      key={item.id}
+      img={item.coverImg}
+      rating={item.stats.rating}
+      reviewCount={item.stats.reviewCount}
+      country={item.location}
+      title={item.title}
+      price={item.price}
+    />
+    )
+  })
 
   return (
     <>
-      <Navbar/>
-      <Hero/>
-      <Card
-        img="katie-zaferes.png"
-        rating={5.0}
-        reviewCount="(6)-"
-        country="USA"
-        title="Life lessons with katie zaferes"
-        price="$136"
-      />
+      <Navbar />
+      <Hero />
+      <section className='card-component'>
+        {cardComponents}
+      </section>
+
     </>
   )
 }
--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,56 +1,32 @@
-import mountainBike from "../assets/mountain-bike.png"
-import starPhoto from "../assets/star.png"
-import weddingPhoto from "../assets/wedding-photography.png"
-
-
+import starImg from "../assets/star.png"
 export default function Card(props) {
-  /*
- <div className="wedding-card">
-                        <img src={weddingPhoto} alt="wedding-photo" />
-                        <div className="card-body">
-                        <div className="card-description">
-                        <img src={starPhoto} alt="star" id="starImage" />
-                        <span className="rating">5.0</span>
-                        <span className="gray-text">(30)-USA</span>
-                        </div>
-                        <p>Learn wedding photography From $125/person</p>
-                      </div>
-                    </div>
-                    <div className="bike-card">
-                        <img src={mountainBike} alt="mountain-bike" />
-                        <div className="card-body">
-                        <div className="card-description">
-                        <img src={starPhoto} alt="star" id="starImage" />
-                        <span className="rating">4.8</span>
-                        <span className="gray-text">(3)-USA</span>
-                        </div>
-                        <p>Group mountian biking From $50/person</p>
-                      </div>
-                    </div>
-                    */
+
+
 
     return (
         <>
             <section className="card-section">
-                <h1>Card component</h1>
-                <div className="card-component">
-                    <div className="katie-card">
-                      <img src={`../assets/{props.img}`} alt="kaite zaferes" width="176px" height="235px"  />
-                      <div className="card-body">
+
+
+                <div className="katie-card">
+                    <img src={`../images/${props.img}`} alt="kaite zaferes" width="176px" height="235px " className="katieImg" />
+                    <div className="card-body">
                         <div className="card-description">
-                        <img src={starPhoto} alt="star" id="starImage" />
-                        <span className="rating">{props.rating}</span>
-                        <span className="reviewCount">{props.reviewCount}</span>
-                        <span className="Country">{props.country}</span>
+                            <img src={starImg} alt="star" id="starImage" />
+                            <span className="rating gray-text">{props.rating}</span>
+                            <span className="reviewCount gray-text">({props.reviewCount})-</span>
+                            <span className="Country gray-text">{props.country}</span>
                         </div>
                         <p>{props.title}</p>
-                        <p>{props.price}/person</p>
-                      </div>
+                        <p><strong>from ${props.price}</strong>/person</p>
                     </div>
-                    
-                   
+
+
+
                 </div>
             </section>
+
         </>
     )
+
 }
