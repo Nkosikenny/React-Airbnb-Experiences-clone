5a32285695e6760454b183b91b7074c660d104a4
nkosikenny <117735539+Nkosikenny@users.noreply.github.com>
2024-06-24 18:03:28 +0000

    updated card file

--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,7 +1,17 @@
 import starImg from "../assets/star.png"
 export default function Card(props) {
 
+    let badgeText;
+    if(props.openSpots===0){
+       badgeText="SOLD OUT";
+    }else if(props.openSpots==="Online"){
+        badgeText="Online";
+    }else{
+        badgeText="";
+    }
 
+  console.log("OpenSpots: " + props.openSpots);
+  console.log("badgeText: " + badgeText);
 
     return (
         <>
@@ -10,6 +20,7 @@ export default function Card(props) {
 
                 <div className="katie-card">
                     <img src={`../images/${props.img}`} alt="kaite zaferes" width="176px" height="235px " className="katieImg" />
+                    {badgeText && <div className="card-badge">{badgeText}</div>}
                     <div className="card-body">
                         <div className="card-description">
                             <img src={starImg} alt="star" id="starImage" />
