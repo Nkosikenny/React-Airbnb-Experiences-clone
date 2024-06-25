e197b02f709289b3565691beb297171ac708be14
nkosikenny <117735539+Nkosikenny@users.noreply.github.com>
2024-06-25 12:11:18 +0000

    Modified Card and  App files

--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,17 +1,16 @@
 import starImg from "../assets/star.png"
 export default function Card(props) {
-
+    
     let badgeText;
-    if(props.openSpots===0){
-       badgeText="SOLD OUT";
-    }else if(props.openSpots==="Online"){
-        badgeText="Online";
-    }else{
-        badgeText="";
+    if (props.item.openSpots === 0) {
+        badgeText = "SOLD OUT";
+    } else if (props.item.openSpots === 27) {
+        badgeText = "Online";
+    } else {
+        badgeText = "";
     }
 
-  console.log("OpenSpots: " + props.openSpots);
-  console.log("badgeText: " + badgeText);
+    console.log(props);
 
     return (
         <>
@@ -19,19 +18,18 @@ export default function Card(props) {
 
 
                 <div className="katie-card">
-                    <img src={`../images/${props.img}`} alt="kaite zaferes" width="176px" height="235px " className="katieImg" />
-                    {badgeText && <div className="card-badge">{badgeText}</div>}
+                    <img src={`../images/${props.item.coverImg}`} alt="kaite zaferes" width="176px" height="235px " className="katieImg" />
+                     {badgeText && <div className="card-badge">{badgeText}</div>}
                     <div className="card-body">
                         <div className="card-description">
                             <img src={starImg} alt="star" id="starImage" />
-                            <span className="rating gray-text">{props.rating}</span>
-                            <span className="reviewCount gray-text">({props.reviewCount})-</span>
-                            <span className="Country gray-text">{props.country}</span>
+                            <span className="rating gray-text">{props.item.stats.rating}</span>
+                            <span className="reviewCount gray-text">({props.item.stats.reviewCount})-</span>
+                            <span className="Country gray-text">{props.item.location}</span>
                         </div>
-                        <p>{props.title}</p>
-                        <p><strong>from ${props.price}</strong>/person</p>
+                        <p>{props.item.title}</p>
+                        <p><strong>from ${props.item.price}</strong>/person</p>
                     </div>
-
 
 
                 </div>
